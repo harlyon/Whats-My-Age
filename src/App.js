b7352b289f50e43e8cf7e8dfcbb97a9661e2d11d
Harrison <harrisonekpobimi@gmail.com>
2018-09-26 08:39:09 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
+import './App.css';
+import Stats from './Stats';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      newDate: '',
+      birthday: '1992-06-21',
+      showStats: false
+    }
+  }
+
+  changeBirthday() {
+    console.log(this.state);
+    this.setState({ 
+      birthday: this.state.newDate,
+      showStats: true
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Form inline>
+          <h2>Input Your Date of Birth!</h2>
+          <FormControl 
+            type="date"
+            onChange={ event => this.setState({ newDate: event.target.value })}
+          >
+          </FormControl>
+          {' '}
+          <Button onClick={() => this.changeBirthday()}>
+            Submit
+          </Button>
+          {
+            this.state.showStats ?
+            <div className="">
+              <Stats date={this.state.birthday} />
+            </div>
+            :
+            <div></div>
+          }
+            
+        </Form>
+      </div>
+    )
+  }
+}
+
+export default App;
+
---
+++ src/Stats.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Image from './image/image.jpg';
+
+class Stats extends Component {
+
+    timeSince(date) {
+        let today = new Date().getTime();
+        let other_date = new Date(date).getTime();
+        let difference = Math.abs(today - other_date);
+    
+        let days = Math.floor(difference / (1000*3600*24));
+        let years = Math.floor(days/365);
+        days -= years*365;
+        let months = Math.floor(days/31);
+        days -= months*31;
+    
+        return `${years} years, ${months} months, and ${days} days`
+      }
+    
+      render() {
+        return (
+          <div>
+            <h3>{this.props.date}</h3>
+            <h4>Congrats on {this.timeSince(this.props.date)}!</h4>
+            <img src={Image} alt="party-popper" className="img-fluid img" />
+          </div>
+        )
+    
+ }
+}
+
+export default Stats;
